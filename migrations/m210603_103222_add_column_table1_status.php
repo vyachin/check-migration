c9e426ab68c90214d54a4f7a102718a70e644652
Vladimir Vyachin <vladimir@vyachin.net>
2021-06-03 13:53:34 +0300

    forty commit

--- migrations/m210603_103222_add_column_table1_status.php
+++ migrations/m210603_103222_add_column_table1_status.php
@@ -7,17 +7,11 @@ use yii\db\Migration;
  */
 class m210603_103222_add_column_table1_status extends Migration
 {
-    /**
-     * {@inheritdoc}
-     */
     public function safeUp()
     {
         $this->addColumn('table1', 'status', $this->tinyInteger());
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function safeDown()
     {
         $this->dropColumn('table1', 'status');
